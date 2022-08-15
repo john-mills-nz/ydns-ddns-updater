d946b1c334878a7233790a445c20ce9375afea90
John Mills <309590+john-mills-nz@users.noreply.github.com>
2022-08-15 13:48:03 +1200

    Improved script to have better logging and error handling

--- update-ip-address.sh
+++ update-ip-address.sh
@@ -1,17 +1,55 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]]
+failed=0
+
+# Check arguments
+if [[ $# -lt 2 ]]
   then
-    echo "Must provide domain name to be updated"
+    echo -e "Must provide a credentials file path and one or more domain names to be updated\nusage: ./update-ip-address.sh credentials-file domain-name-1 [domain-name-2 domain-name-3 ...]"
     exit 1
 fi
 
 # Read Credentials
 credentials=$(<$1)
 
-# Update IP address
-echo "Updating IP Address for $2 - $(date)" >> $2.log
-curl --head --user $credentials https://ydns.io/api/v1/update/?host=$2 >> $2.log
+# Update IP addresses
+shift
+for domainName in "$@"
+do
+  echo "Updating IP Address for $domainName - $(date)" >> $domainName.log
+  
+  for ((retryCounter=0; retryCounter<=5; retryCounter++ ))
+  do
+    result=$(curl --user $credentials --silent --show-error --max-time 10 --write-out ":::%{http_code}" https://ydns.io/api/v1/update/?host=$domainName)
+    statusCode=${result##*\:::}
+    message=${result%%\:::*}
+    
+    if [[ statusCode -eq 200 ]]
+      then
+        break
+    fi
+    
+    sleep $((retryCounter * retryCounter))
+    
+  done
+  
+  echo -e "Status: ${statusCode}\nResponse: ${message}\nRetries: ${retryCounter}\n" >> $domainName.log
+ 
+  # Trim log file
+  echo "$(tail -n 5000 $domainName.log)" > $domainName.log
+  
+  if [[ statusCode -ge 400 ]]
+    then
+      failed=1
+      failureStatusCode=statusCode
+      failureMessage=message
+  fi
+  
+done
 
-# Trim log file
-echo "$(tail -n 5000 $2.log)" > $2.log
+# Report failure if necessary
+if [[ failed -eq 1 ]]
+  then
+    echo -e "${failureStatusCode} - ${failureMessage}"
+    exit 1
+fi
